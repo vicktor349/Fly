354a658874f1a008c91cf51c9beec14fde8f8ec0
vicktor349 <ayomidevictor349@gmail.com>
2023-06-14 22:44:00 +0100

    Made it responsive and cleaned code

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,10 +5,12 @@ import Logo from '../public/images/logo/logo.png'
 import { CiMenuBurger } from 'react-icons/ci'
 import { useDisclosure } from '@mantine/hooks';
 import { Drawer, Button } from '@mantine/core';
+import { useSession, signIn, signOut } from 'next-auth/react'
 
 
 const Navbar = () => {
     const [opened, { open, close }] = useDisclosure(false);
+    const { data: session } = useSession()
     return (
         <div className='flex justify-end items-end bg-[#7AF687]'>
             <div className='mr-auto mx-24 md:ml-16 sm:ml-12 ssm:ml-4'>
@@ -26,9 +28,15 @@ const Navbar = () => {
                 <p className='text-xl font-bold text-white'>ride in comfort & safety</p>
             </div>
             <div className='flex justify-center items-center mb-10 mr-24 md:mr-16 md:inline-block ssm:hidden'>
-                <Button variant="outline" color="green">
-                    Sign In
-                </Button>
+                {session ?
+                    <Button variant="outline" color="green" onClick={() => signOut()}>
+                        Sign Out
+                    </Button>
+                    :
+                    <Button variant="outline" color="green" onClick={() => signIn()}>
+                        Sign In
+                    </Button>
+                }
             </div>
             <div className='flex justify-center items-center cursor-pointer mr-16 text-3xl font-bold mb-8 md:hidden sm:inline-block ssm:mr-4'>
                 <CiMenuBurger
@@ -37,7 +45,7 @@ const Navbar = () => {
                 />
                 <Drawer opened={opened} onClose={close} position='right' size='50%' overlayProps={{ opacity: "0.55", blur: "10" }}>
                     <Button variant='outline' color='green' className='mt-5 flex mx-auto'>
-                        Sign In
+                        Home
                     </Button>
                 </Drawer>
             </div>
