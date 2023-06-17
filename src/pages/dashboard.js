f316be85a2c34b7c879c94dc774070e9c7fc4889
vicktor349 <ayomidevictor349@gmail.com>
2023-06-17 19:22:44 +0100

    Made changes

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -11,8 +11,6 @@ import axios from 'axios';
 import { getSession, useSession } from 'next-auth/react';
 import PreLoader from '@/components/PreLoader';
 import { Loader } from '@mantine/core';
-import { Notification } from '@mantine/core';
-import { IconCheck } from '@tabler/icons-react';
 import { AiFillCar } from 'react-icons/ai'
 import { CgProfile } from 'react-icons/cg'
 
